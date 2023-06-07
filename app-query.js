8ee88d1d93959125b529d078f1515397858400b7
Nicolas Xavier Herrera Medina <nicoxavier2013@hotmail.com>
2023-06-07 09:43:59 -0500

    Correcion por sofi

--- app-query.js
+++ app-query.js
@@ -1,34 +1,56 @@
-const express = require("express");
-const cors= require('cors');
-const app = express();
+const express  = require("express");  //importar
+const cors = require('cors');
+const app = express() //llamar la libreria
 
-const port = 3001;
-app.get('/',function(req,res){
-    res.send('Programacion Web')
-}
-)
+const port = 3001;  //puerto
+
+
 app.use(cors({
-    origin: 'http://localhost:3000',
-    optionSuccessStatus: 200
+    origin : "http://localhost:3000",
+    optionsSuccessStatus:200
 }))
+
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() ); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
 extended: true
 }));
+
+app.get('/', function(req,res){   //ruta
+    res.send('Programcion Web');  //enviar al usuario un mensaje
+})
+
+
+/*app.get('/alumnos/:codigo/:nombre',function(req,res){
+    var codigo = req.params.codigo;
+    var nombre = req.params.nombre;
+    res.send(`<h1>Codigo: ${codigo}</h1> <h2>Nombre: ${nombre} </h2>`);
+})*/
+
+app.get('/alumnos-query', function(req, res) {
+    var codigo = req.query.cod;
+    var nombre = req.query.nom;
+    res.send(`Tipo QUERY<br/><h1>Codigo: ${codigo}</h1>
+    <h2>Nombre: ${nombre}</h2>`);
+    });
+
+app.post('/alumnos-form', function(req, res){
+    var codigo = req.body.codigo;
+    var nombre = req.body.nombre;
+    var apellidos = req.body.apellidos;
+    var comentario = req.body.comentario;
+    //res.send(`Tipo FORM<br/><h1>Codigo: ${codigo}</h1>
+    //<h2>Nombre Completo: ${nombre} __ ${apellidos}</h2>
+    // <h3>Comentario: ${comentario}</h3>`);
+    var obj={"codigo": "1245",
+     "nombre":"sasasa"
+        
+    }
+    res.send(obj);
+    });
+       
+
+//cofigurar el servidor cómo vaa estar escuchando los mensajes
 app.listen(port, function(){
-    console.log(`Servidor funcionando en el puerto ${port}`)
-}
-)
-app.get('/alumnos-query',function(req,res){
-    var codigo=req.query.cod;
-    var nombre=req.query.nom;
-    res.send(`<h1>Tipo QueryCodigo:${codigo}</h1><h2>Nombre: ${nombre}</h2>`);
-}
-)
-app.post('/alumnos-form',function(req,res){
-    var codigo=req.body.codigo;
-    var nombre=req.body.nombre;
-    res.send(`<h1>Tipo Form Codigo:${codigo}</h1><h2>Nombre: ${nombre}</h2>`);
-}
-)
+    console.log(`Servidor está funcionando en el puerto ${port}`)  // `
+})
